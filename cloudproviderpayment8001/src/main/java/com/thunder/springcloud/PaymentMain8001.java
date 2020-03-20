5e8fbb7a7f9572a8115bb947a465e11ef3360e01
WeAreThunder <=>
2020-03-20 16:40:50 +0800

    2020.03.20

--- cloudproviderpayment8001/src/main/java/com/thunder/springcloud/PaymentMain8001.java
+++ cloudproviderpayment8001/src/main/java/com/thunder/springcloud/PaymentMain8001.java
@@ -3,10 +3,13 @@ package com.thunder.springcloud;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
 //指定mapper文件的位置，这样mapper文件不加mapper注解也能正常运行
 @MapperScan("com.thunder.springcloud.mapper")
+//我是服务的申请者
+@EnableEurekaClient
 public class PaymentMain8001 {
     public static void main(String[] args) {
         SpringApplication.run(PaymentMain8001.class,args);
